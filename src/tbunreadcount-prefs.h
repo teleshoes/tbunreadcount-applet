e08ede9b78942047b102c6015492b2c84c96a6f5
Elliot Wolk <elliot.wolk@gmail.com>
2011-03-20 21:17:44 -0400

    purging tpbattstat-applet; removed prefs aside from delay

--- src/tbunreadcount-prefs.h
+++ src/tbunreadcount-prefs.h
@@ -25,70 +25,8 @@
 
 #define SCHEMA_DIR "/schemas/apps/tbunreadcount_applet/prefs"
 
-enum DischargeStrategy {
-    DISCHARGE_SYSTEM, /*
-       -never force, let the system decide
-         {this probably means BAT1 all the way, then BAT0 all the way}*/
-
-    DISCHARGE_LEAPFROG, /*
-       -if one battery was forcibly being discharging,
-         forcibly discharge it unless that battery has rem_percent that
-         is less than the other by an amount greater than the threshold
-       -otherwise, force the battery with larger rem_percent
-       -will never forcibly discharge a battery with rem_percent less
-         than the threshold
-        e.g: (threshold=3)
-          50 49 48 47 46......................46 45 44 43 42 41 40 39 38
-            ^  ^  ^  ^  v  v  v  v  v  v  v  v  ^  ^  ^  ^  ^  ^  ^  ^
-          50..........50 49 48 47 46 45 44 43 42......................42*/
-
-    DISCHARGE_CHASING /*
-       -if one battery has rem_percent that is bigger than the other,
-         force that battery to discharge
-       -otherwise, allow the system default to discharge */
-};
-
-enum ChargeStrategy {
-    CHARGE_SYSTEM, /*
-       -never inhibit charge, let the system decide
-         {this probably means keep charging one of them to full,
-          then charge the other one. BAT0 is probably selected if
-          both batteries are present}*/
-
-    CHARGE_LEAPFROG, /*
-       -if one battery was charging, inhibit the other for 1 minute,
-         unless the charging battery has rem_percent that
-         is greater than the other by an amount greater than the threshold.
-         in that case, inhibit the currently charging battery for 1 minute
-       -otherwise, inhibit the battery with a larger rem_percent for
-         1 minute */
-
-    CHARGE_CHASING, /*
-       -inhibit the batter with a higher rem_percent,
-         uninhibit the other*/
-
-    CHARGE_BRACKETS /*
-       -for each bracket {sorted lowest-first}:
-         -charge the preferred battery until its at or above the bracket
-         -charge the other battery until its at or above the bracket
-       -if both batteries are above the top bracket, dont inhibit charge*/
-};
-
 typedef struct {
     int delay;
-    enum DischargeStrategy dischargeStrategy;
-    int dischargeLeapfrogThreshold;
-    enum ChargeStrategy chargeStrategy;
-    int chargeLeapfrogThreshold;
-    int chargeBracketsPrefBattery;
-    int *chargeBrackets;
-    int chargeBracketsSize;
-
-    int displayPowerAvg;
-    int displayColoredText;
-    int displayIcons;
-    int displayOnlyOneIcon;
-    int displayBlinkingIndicator;
 } Prefs;
 
 void load_prefs (PanelApplet *applet, Prefs *prefs);
